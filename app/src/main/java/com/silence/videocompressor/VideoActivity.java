cab1c0800a568359ec2db1f6900fe8decdeb5fdb
silence <yangkemuyi@sina.com>
2020-04-02 18:38:11 +0800

    Android自带视频压缩方式

---
+++ app/src/main/java/com/silence/videocompressor/VideoActivity.java
@@ -0,0 +1,22 @@
+package com.silence.videocompressor;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoActivity extends AppCompatActivity {
+
+    private VideoView vvVideo;
+    String geturl="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        vvVideo= (VideoView) findViewById(R.id.vvVideo);
+        geturl=getIntent().getStringExtra("vvVideo");
+        vvVideo.setVideoPath(geturl);
+        vvVideo.start();
+    }
+}
